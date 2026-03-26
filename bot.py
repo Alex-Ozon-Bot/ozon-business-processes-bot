8343f02d615144ec85a009c3b20b89e0d545571f
TechPack <sales@techpack.by>
2026-03-26 16:45:01 +0300

    update link 4 26.03.2026

--- bot.py
+++ bot.py
@@ -466,7 +466,7 @@ async def send_processes_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE)
     try:
         # Создаем клавиатуру с кнопками
         keyboard = [
-            [InlineKeyboardButton("📦 Официальная инструкция Ozon (ПВЗ Беларусь)", url="https://univer.ozon.ru/knowledge-base")]
+            [InlineKeyboardButton("📦 Официальная инструкция Ozon (ПВЗ Беларусь)", url="https://univer.ozon.ru")]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         
@@ -592,7 +592,7 @@ async def send_pdf_callback(query, context):
         
         # Создаем клавиатуру с кнопками
         keyboard = [
-            [InlineKeyboardButton("📦 Официальная инструкция Ozon (ПВЗ Беларусь)", url="https://https://univer.ozon.ru/knowledge-base")]
+            [InlineKeyboardButton("📦 Официальная инструкция Ozon (ПВЗ Беларусь)", url="https://https://univer.ozon.ru")]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         
